7a94a2122c3e883c85d957e455f5c74358f57d59
Gabriel Jesus <gabriel.developer2@gmail.com>
2024-02-08 10:44:51 -0300

    create home list product and screen PDP

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,14 +1,26 @@
 
 import { CategoryButton } from '@/components/Category-Button'
 import { Header } from '@/components/Header'
-import { CATEGORIES } from '@/utils/data/products'
-import { useState } from 'react'
-import { FlatList, View } from 'react-native'
+import { Product } from '@/components/Product'
+import { CATEGORIES, MENU } from '@/utils/data/products'
+import { Link } from 'expo-router'
+import { useRef, useState } from 'react'
+import { FlatList, SectionList, Text, View } from 'react-native'
 
 export default function Home() {
   const [category, setCategory] = useState(CATEGORIES[0])
+  const sectionListRef = useRef<SectionList>(null)
   function handleCategorySelect(selectedCategory: string) {
     setCategory(selectedCategory)
+    const sectionIndex = CATEGORIES.findIndex((category) => category === selectedCategory)
+    if (sectionListRef.current) {
+      sectionListRef.current.scrollToLocation({
+        animated: true,
+        sectionIndex,
+        itemIndex: 0
+      })
+    }
+
   }
 
   return (
@@ -31,6 +43,30 @@ export default function Home() {
         className='max-h-10 mt-5'
         contentContainerStyle={{ gap: 12, paddingHorizontal: 20 }}
         showsHorizontalScrollIndicator={false}
+      />
+      <SectionList
+        ref={sectionListRef}
+        sections={MENU}
+        keyExtractor={item => item.id}
+        stickySectionHeadersEnabled={false}
+        renderItem={({ item }) => (
+          <Link
+            href={`/product/${item.id}`}
+            asChild
+          >
+            <Product
+              data={item}
+            />
+          </Link>
+        )}
+        renderSectionHeader={({ section: { title } }) => (
+          <Text className='text-xl text-white font-heading mt-8 mb-3'>{title}</Text>
+        )}
+        className='flex-1 p-5'
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={{
+          paddingBottom: 100
+        }}
       />
     </View>
   )
---
+++ src/utils/functions/format-currency.ts
@@ -0,0 +1,7 @@
+
+export function formatCurrency(value: number) {
+  return value.toLocaleString("pt-BR", {
+    style: 'currency',
+    currency: 'BRL'
+  })
+}
